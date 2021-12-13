0200fc9ecc89f0215587cfc836acd0ddb91d01b3
Simon Brent <sb23@sanger.ac.uk>
2021-12-13 12:19:29 +0000

    eslint-plugin-vue/no-side-effects-in-computed-properties does not report errors for this. or this.

---
+++ LintTest.vue
@@ -0,0 +1,12 @@
+<script>
+  export default {
+    data: () => ({ bar: 1 }),
+    computed: {
+      foo: function () {
+        this.$set(this, 'bar', 1);
+        this.$nextTick(() => this.$set(this, 'bar', 2));
+        return 1;
+      }
+    }
+  };
+</script>
